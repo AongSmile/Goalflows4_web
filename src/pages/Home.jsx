88a8b4ae64cb2014546132b0c69f428619491949
AongSmile <nasee00010@gmail.com>
2026-09-11 13:48:04 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     <>
       {/* <Navbar /> */}
       <Header />
-      <Productforsale />
+      {/* <Productforsale /> */}
       <Productsoffered/>
       <Community />
       <OurCustomer/>
